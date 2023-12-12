589ea81a8322a28be84825de41b2585c30c661d7
otalorajuand <otalorajuand@gmail.com>
2023-12-12 14:56:37 -0500

    Addition of requirements

--- chat_app.py
+++ chat_app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import time
 
 st.set_page_config(
-    page_title="Pregúntale a La Casa Museo", page_icon=":bird:")
+    page_title="Pregúntale a La Casa Museo", page_icon=":house:")
 
 from prompt_generation.output_generator import output_generator
 from prompt_generation.augmented_prompt import augment_prompt_generator
 
 
-st.title("Pregúntale a La Casa Museo :bird:")
+st.title("Pregúntale a La Casa Museo :house:")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -33,8 +33,7 @@ if prompt := st.chat_input("Haznos una pregunta"):
         full_response = ""
         augment_prompt, documents = augment_prompt_generator(prompt)
         output = output_generator(augment_prompt)
-        assistant_response = output[0]["generated_text"][len(
-            augment_prompt) + 1:]
+        assistant_response = output[0]["generated_text"][len(augment_prompt) + 1:]
         # Simulate stream of response with milliseconds delay
         for chunk in assistant_response.split():
             full_response += chunk + " "
