d79e91cf48b1c3658838a8052425b4005e339a21
otalorajuand <otalorajuand@gmail.com>
2023-12-13 10:56:28 -0500

    Test

--- chat_app.py
+++ chat_app.py
@@ -18,11 +18,12 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+        st.caption(message["documents"])
 
 # Accept user input
 if prompt := st.chat_input("Haznos una pregunta"):
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "user", "content": prompt, "documents": ""})
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -34,8 +35,10 @@ if prompt := st.chat_input("Haznos una pregunta"):
         with st.spinner("Generando respuesta..."):
             augment_prompt, documents = augment_prompt_generator(prompt)
             output = output_generator(augment_prompt)
-            st.write(output)
-            assistant_response = output[0]["generated_text"][len(augment_prompt) + 1:]
+            try:
+                assistant_response = output[0]["generated_text"][len(augment_prompt) + 1:]
+            except:
+                st.error('Revisa tu conexión a internet. Intentalo más tarde.')
 
         # Simulate stream of response with milliseconds delay
         for chunk in assistant_response.split():
@@ -47,4 +50,4 @@ if prompt := st.chat_input("Haznos una pregunta"):
         st.caption(documents)
     # Add assistant response to chat history
     st.session_state.messages.append(
-        {"role": "assistant", "content": full_response})
+        {"role": "assistant", "content": full_response, "documents": documents})
